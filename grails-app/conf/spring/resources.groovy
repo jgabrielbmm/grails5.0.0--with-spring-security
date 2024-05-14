ee714d814ddf048487adf31145793edb1ae752fa
Joao Gabriel <joaogabriel.bmm@gmail.com>
2024-05-14 14:19:39 -0300

    feat: initial project with spring-security

---
+++ grails-app/conf/spring/resources.groovy
@@ -0,0 +1,5 @@
+import coredomain.user.UserPasswordEncoderListener
+// Place your Spring DSL code here
+beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
+}
---
+++ grails-app/init/security/BootStrap.groovy
@@ -0,0 +1,30 @@
+package security
+import coredomain.user.User
+import coredomain.user.Role
+import coredomain.user.UserRole
+import grails.gorm.transactions.Transactional
+
+class BootStrap {
+    def init = {
+        addTestUser()
+    }
+
+    @Transactional
+    void addTestUser() {
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+
+        def testUser = new User(username: 'me', password: 'password').save()
+
+        UserRole.create testUser, adminRole
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
+        assert User.count() == 1
+        assert Role.count() == 1
+        assert UserRole.count() == 1
+    }
+}
+
